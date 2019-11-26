4de50905a24f44be495f8a9a9bf09b0a6c5997ca
cabra <cabra@DESKTOP-FVTS8NC>
2019-11-25 23:36:03 -0400

    En este punto ya se a agregado la agenda  en dos partes eventos y agenda una tiene los contactos(agenda) y el otro las fechas y eventos (eventos ) estoy haciendo commit rapido por que ya habia hecho el programa de ante mano solo que no sabia usar el github

---
+++ WebApplication1/WebApplication1/Controllers/contactoesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class contactoesController : Controller
+    {
+        private estudiantesEntities db = new estudiantesEntities();
+
+        // GET: contactoes
+        public ActionResult Index(string Nombre_contacto, string celular)
+        {
+            var lista = from data in db.contactoes
+                        select data;
+            if (!string.IsNullOrEmpty(Nombre_contacto))
+            {
+                lista = lista.Where(a => a.Nombre_contacto.Contains(Nombre_contacto));
+            }
+
+            if (!string.IsNullOrEmpty(celular))
+            {
+                lista = lista.Where(a => a.celular.Contains(celular));
+            }
+
+            return View(lista);
+        }
+
+        // GET: contactoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = db.contactoes.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // GET: contactoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: contactoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nombre_contacto,celular,email,direccion")] contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contactoes.Add(contacto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+        // GET: contactoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = db.contactoes.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: contactoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nombre_contacto,celular,email,direccion")] contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: contactoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = db.contactoes.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: contactoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            contacto contacto = db.contactoes.Find(id);
+            db.contactoes.Remove(contacto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/horariosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class horariosController : Controller
+    {
+        private estudiantesEntities db = new estudiantesEntities();
+
+        // GET: horarios
+        public ActionResult Index(string evento, string fecha)
+        {
+            var lista = from data in db.horarios
+                        select data;
+            if (!string.IsNullOrEmpty(evento))
+            {
+                lista = lista.Where(a => a.evento.Contains(evento));
+            }
+
+            if (!string.IsNullOrEmpty(fecha))
+            {
+                lista = lista.Where(a => a.fecha.Contains(fecha));
+            }
+
+            return View(lista);
+        }
+        // GET: horarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            horario horario = db.horarios.Find(id);
+            if (horario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(horario);
+        }
+
+        // GET: horarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: horarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,evento,fecha,hora,direccion")] horario horario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.horarios.Add(horario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(horario);
+        }
+
+        // GET: horarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            horario horario = db.horarios.Find(id);
+            if (horario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(horario);
+        }
+
+        // POST: horarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,evento,fecha,hora,direccion")] horario horario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(horario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(horario);
+        }
+
+        // GET: horarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            horario horario = db.horarios.Find(id);
+            if (horario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(horario);
+        }
+
+        // POST: horarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            horario horario = db.horarios.Find(id);
+            db.horarios.Remove(horario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
